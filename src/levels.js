bc7095196f416d83618a4256b245a707ff234042
Levi Durfee <levi.durfee@gmail.com>
2017-04-12 23:20:08 -0400

    create random levels class (#12)

---
+++ src/levels.js
@@ -0,0 +1,15 @@
+/*
+* random levels may be fun. they should use a seed of some sort, so they
+* can be shared/recreated. it's no fun if you can't share a challenging
+* level with your friends.
+ */
+class Levels {
+    constructor() {
+        // yayy
+    }
+
+    prng() {
+        // generate some randomness
+    }
+}
+
